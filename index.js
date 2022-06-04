c270b02d1a8649497ae7b67259742c5fc42e4327
malmz90 <70096644+malmz90@users.noreply.github.com>
2022-06-04 23:22:22 +0200

    mariadb connected

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require('express')
-
-const PORT = process.env.PORT || 3001
-
+const cors = require('cors')
+const PORT = process.env.PORT || 4000
 const app = express()
+const mariadb = require('mariadb')
 
-app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from server!' })
+const pool = mariadb.createPool({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'root',
+  connectionLimit: 5,
+  database: 'arenan',
 })
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`)
+pool.getConnection().then((db) => {
+  app.use(cors())
+
+  app.get('/api', async (req, res) => {
+    const users = await db.query('SELECT * FROM users')
+    res.json(users)
+  })
+
+  app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`)
+  })
 })
