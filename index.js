7ec753868159b46fbf29117b904e0f4f2e716ce4
malmz90 <70096644+malmz90@users.noreply.github.com>
2022-10-21 19:51:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const PORT = process.env.PORT || 4000
-const app = express()
-const mariadb = require('mariadb')
-const { genPassword, verifyJwt } = require('./utils')
-const bcrypt = require('bcrypt')
-const cookieParser = require('cookie-parser')
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const PORT = process.env.PORT || 4000;
+const app = express();
+const mariadb = require('mariadb');
+const { genPassword, verifyJwt } = require('./utils');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
 
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
-  credentials: true,
-  optionSuccessStatus: 200,
-}
+	origin: 'http://localhost:3000',
+	credentials: true,
+	optionSuccessStatus: 200,
+};
 
 const pool = mariadb.createPool({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'root',
-  connectionLimit: 5,
-  database: 'arenan',
-})
+	host: 'localhost',
+	port: 3306,
+	user: 'root',
+	password: 'root',
+	connectionLimit: 5,
+	database: 'arenan',
+});
 
-app.use(express.urlencoded({ limit: '100mb', extended: true }))
-app.use(express.json({ limit: '100mb' }))
-app.use(cookieParser())
-app.use(cors(corsOptions))
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(cookieParser());
+app.use(cors(corsOptions));
 
 pool.getConnection().then((db) => {
-  app.post('/verifyAuth', verifyJwt, async (req, res) => {
-    const { id } = req.jwt
-    try {
-      const user = await db.query(
-        `
+	app.post('/verifyAuth', verifyJwt, async (req, res) => {
+		const { id } = req.jwt;
+		try {
+			const user = await db.query(
+				`
             SELECT
                 users.id,
                 users.email,
@@ -45,137 +45,150 @@ pool.getConnection().then((db) => {
             WHERE
                 users.id = ?
         `,
-        [id]
-      )
-      res.json({ succes: true, user })
-    } catch (error) {
-      console.log(error)
-    }
-    db.release()
-  })
-  app.get('/items', verifyJwt, async (req, res) => {
-    try {
-      const items = await db.query(
-        `SELECT * FROM items
+				[id]
+			);
+			res.json({ succes: true, user });
+		} catch (error) {
+			console.log(error);
+		}
+		db.release();
+	});
+	app.get('/items', verifyJwt, async (req, res) => {
+		try {
+			const items = await db.query(
+				`SELECT * FROM items
         `
-      )
-      res.json(items)
-    } catch (err) {
-      console.log(err)
-      res.status(500).send({
-        message: 'Something went wrong when trying to fetch items',
-      })
-    }
-    db.release()
-  })
+			);
+			res.json(items);
+		} catch (err) {
+			console.log(err);
+			res.status(500).send({
+				message: 'Something went wrong when trying to fetch items',
+			});
+		}
+		db.release();
+	});
 
-  app.post('/buy/item/:id', verifyJwt, async (req, res) => {
-    const { id: user_id } = req.jwt
-    const { id: itemId } = req.params
-    console.log(itemId)
+	app.post('/buy/item', verifyJwt, async (req, res) => {
+		const { id: user_id } = req.jwt;
+		const { id: itemId } = req.body;
 
-    const [item] = await db.query('SELECT * FROM items WHERE id = ? ', [itemId])
-    if (!item) {
-      res.status(404).json({ succes: false, error: 'No item in database' })
-    }
+		const [item] = await db.query('SELECT * FROM items WHERE id = ? ', [
+			itemId,
+		]);
+		if (!item) {
+			res.status(404).json({
+				succes: false,
+				error: 'No item in database',
+			});
+			return;
+		}
 
-    const [row] = await db.query(
-      'SELECT gold FROM gladiator WHERE user_id = ?',
-      [user_id]
-    )
-    if (item.price > row.gold) {
-      res.status(402).json({ succes: false, message: 'insufficient funds' })
-    }
+		const [row] = await db.query(
+			'SELECT gold FROM gladiator WHERE user_id = ?',
+			[user_id]
+		);
+		if (item.price > row.gold) {
+			res.status(402).json({
+				succes: false,
+				message: 'insufficient funds',
+			});
+			return;
+		}
 
-    const newGoldAmount = row.gold - item.price
+		const newGoldAmount = row.gold - item.price;
 
-    await db.query('UPDATE gladiator SET gold = ?', [newGoldAmount])
+		const [gladiatorRow] = await db.query(
+			'SELECT id FROM gladiator WHERE user_id = ?',
+			[user_id]
+		);
 
-    const [gladiatorRow] = await db.query(
-      'SELECT id FROM gladiator WHERE user_id = ?',
-      [user_id]
-    )
+		await db.query('UPDATE gladiator SET gold = ? WHERE id  = ?', [
+			newGoldAmount,
+			gladiatorRow.id,
+		]);
 
-    await db.query(
-      'INSERT into gladiator_items (gladiator_id,item_id) VALUES (?,?)',
-      [gladiatorRow.id, itemId]
-    )
+		await db.query(
+			'INSERT into gladiator_items (gladiator_id,item_id) VALUES (?,?)',
+			[gladiatorRow.id, itemId]
+		);
 
-    res.json({ succes: true, item: item })
-    db.release()
-  })
+		res.json({ succes: true, item: item });
+		db.release();
+	});
 
-  app.post('/gladiator', verifyJwt, async (req, res) => {
-    const { id } = req.jwt
-    const {
-      name,
-      race,
-      gender,
-      strength,
-      agility,
-      health,
-      axe,
-      sword,
-      hammer,
-      staff,
-      shield,
-      spear,
-      chain,
-      initiative,
-      dodge,
-    } = req.body
-    console.log(req.body)
-    let calculatedHp = Math.ceil(health * 5)
-    try {
-      hammer, staff, shield, spear, chain, initiative, dodge
-      const { insertId } = await db.query(
-        `INSERT INTO gladiator (user_id,name,gender,axe,sword,hammer,staff,shield,spear,chain,initiative,dodge, race,strength,agility,health,hp) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`,
-        [
-          id,
-          name,
-          gender,
-          axe,
-          sword,
-          hammer,
-          staff,
-          shield,
-          spear,
-          chain,
-          initiative,
-          dodge,
-          race,
-          strength,
-          agility,
-          health,
-          calculatedHp,
-        ]
-      )
-      const newGladiator = await db.query(
-        'SELECT * FROM gladiator WHERE gladiator.id = ?',
-        [insertId]
-      )
-      res.json(newGladiator[0])
-    } catch (err) {
-      console.log(err)
-      res.status(500).send({
-        message: 'Something went wrong when trying to create new gladiator',
-      })
-    }
-    db.release()
-  })
+	app.post('/gladiator', verifyJwt, async (req, res) => {
+		const { id } = req.jwt;
+		const {
+			name,
+			race,
+			gender,
+			strength,
+			agility,
+			health,
+			axe,
+			sword,
+			hammer,
+			staff,
+			shield,
+			spear,
+			chain,
+			initiative,
+			dodge,
+		} = req.body;
+		console.log(req.body);
+		let calculatedHp = Math.ceil(health * 5);
+		try {
+			hammer, staff, shield, spear, chain, initiative, dodge;
+			const { insertId } = await db.query(
+				`INSERT INTO gladiator (user_id,name,gender,axe,sword,hammer,staff,shield,spear,chain,initiative,dodge, race,strength,agility,health,hp) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`,
+				[
+					id,
+					name,
+					gender,
+					axe,
+					sword,
+					hammer,
+					staff,
+					shield,
+					spear,
+					chain,
+					initiative,
+					dodge,
+					race,
+					strength,
+					agility,
+					health,
+					calculatedHp,
+				]
+			);
+			const newGladiator = await db.query(
+				'SELECT * FROM gladiator WHERE gladiator.id = ?',
+				[insertId]
+			);
+			res.json(newGladiator[0]);
+		} catch (err) {
+			console.log(err);
+			res.status(500).send({
+				message:
+					'Something went wrong when trying to create new gladiator',
+			});
+		}
+		db.release();
+	});
 
-  app.get('/logout', (req, res) => {
-    res.cookie('arenan_token', '', { maxAge: 1 })
-    res.json()
-    db.release()
-  })
+	app.get('/logout', (req, res) => {
+		res.cookie('arenan_token', '', { maxAge: 1 });
+		res.json();
+		db.release();
+	});
 
-  app.post('/login', async (req, res) => {
-    const { email, password } = req.body
+	app.post('/login', async (req, res) => {
+		const { email, password } = req.body;
 
-    try {
-      const users = await db.query(
-        `
+		try {
+			const users = await db.query(
+				`
             SELECT
               *
             FROM 
@@ -183,80 +196,80 @@ pool.getConnection().then((db) => {
             WHERE
                 users.email = ?
         `,
-        [email]
-      )
+				[email]
+			);
 
-      if (!users.length) {
-        return res.json({ message: 'No user found' })
-      }
+			if (!users.length) {
+				return res.json({ message: 'No user found' });
+			}
 
-      const user = users[0]
-      const dbPassword = `$2b$${user.password.slice(4)}`
+			const user = users[0];
+			const dbPassword = `$2b$${user.password.slice(4)}`;
 
-      const gladiators = await db.query(
-        'SELECT * FROM gladiator WHERE user_id = ? ',
-        [user.id]
-      )
-      const gladiator = gladiators[0]
-      db.release()
-      if (bcrypt.compareSync(password, dbPassword)) {
-        const token = jwt.sign(
-          { id: user.id },
-          process.env.ACCES_TOKEN_SECRET,
-          { expiresIn: '4h' }
-        )
-        res.cookie('arenan_token', token)
-        return res.json({
-          succes: true,
-          user: user,
-          gladiator: gladiator,
-          token: 'Bearer ' + token,
-        })
-      } else return res.json({ message: 'password/username invalid' })
-    } catch (error) {
-      console.log(error)
-      return res.json({ err: error })
-    }
-  })
+			const gladiators = await db.query(
+				'SELECT * FROM gladiator WHERE user_id = ? ',
+				[user.id]
+			);
+			const gladiator = gladiators[0];
+			db.release();
+			if (bcrypt.compareSync(password, dbPassword)) {
+				const token = jwt.sign(
+					{ id: user.id },
+					process.env.ACCES_TOKEN_SECRET,
+					{ expiresIn: '4h' }
+				);
+				res.cookie('arenan_token', token);
+				return res.json({
+					succes: true,
+					user: user,
+					gladiator: gladiator,
+					token: 'Bearer ' + token,
+				});
+			} else return res.json({ message: 'password/username invalid' });
+		} catch (error) {
+			console.log(error);
+			return res.json({ err: error });
+		}
+	});
 
-  app.post('/register', async function (req, res) {
-    const hashedPassword = await genPassword(req.body.password)
-    console.log(req.body)
-    const { email } = req.body
-    if (!email) {
-      return res
-        .status(400)
-        .send({ success: false, error: 'Missing input parameters.' })
-    }
-    try {
-      const queryResult = await db.query(
-        'INSERT INTO users(email,password) VALUES( ?, ? )',
-        [email, hashedPassword]
-      )
-      const id = Number(queryResult.insertId)
-      const user = {
-        id: id,
-        email: email,
-      }
+	app.post('/register', async function (req, res) {
+		const hashedPassword = await genPassword(req.body.password);
+		console.log(req.body);
+		const { email } = req.body;
+		if (!email) {
+			return res
+				.status(400)
+				.send({ success: false, error: 'Missing input parameters.' });
+		}
+		try {
+			const queryResult = await db.query(
+				'INSERT INTO users(email,password) VALUES( ?, ? )',
+				[email, hashedPassword]
+			);
+			const id = Number(queryResult.insertId);
+			const user = {
+				id: id,
+				email: email,
+			};
 
-      res.json(user)
-    } catch (error) {
-      console.log('errfart', { error })
-      let userError = 'Ett okänt fel inträffade.'
-      if (error.code === 'ER_DUP_ENTRY') {
-        userError = 'E-postadressen eller användarnamn används redan'
-      }
-      return res.json({ isAuthenticated: false, error: userError })
-    }
-    db.release()
-  })
+			res.json(user);
+		} catch (error) {
+			console.log('errfart', { error });
+			let userError = 'Ett okänt fel inträffade.';
+			if (error.code === 'ER_DUP_ENTRY') {
+				userError = 'E-postadressen eller användarnamn används redan';
+			}
+			return res.json({ isAuthenticated: false, error: userError });
+		}
+		db.release();
+	});
 
-  app.get('/api', async (req, res) => {
-    const users = await db.query('SELECT * FROM users')
-    res.json(users)
-  })
+	app.get('/api', async (req, res) => {
+		const users = await db.query('SELECT * FROM users');
+		res.json(users);
+	});
 
-  app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`)
-  })
-})
+	app.listen(PORT, () => {
+		console.log(`Server listening on ${PORT}`);
+	});
+});
