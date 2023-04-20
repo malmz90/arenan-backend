fae78389bcd4fc395d3e7c43bd0b1c76c6b52fcd
malmz90 <70096644+malmz90@users.noreply.github.com>
2023-04-20 21:46:21 +0200

    changes to new db incoming

--- utils.js
+++ utils.js
@@ -1,35 +1,36 @@
-const bcrypt = require('bcrypt')
-const jsonwebtoken = require('jsonwebtoken')
+const bcrypt = require("bcrypt");
+const jsonwebtoken = require("jsonwebtoken");
+
 function genPassword(password) {
-  return bcrypt.hash(password, 10)
+  return bcrypt.hash(password, 10);
 }
 
 function verifyJwt(req, res, next) {
-  const token = req.cookies.arenan_token
+  const token = req.cookies.arenan_token;
   if (token) {
     try {
       const verification = jsonwebtoken.verify(
         token,
         process.env.ACCES_TOKEN_SECRET,
         {
-          algorithms: ['HS256'],
+          algorithms: ["HS256"],
         }
-      )
-      req.jwt = verification
-      next()
+      );
+      req.jwt = verification;
+      next();
     } catch (err) {
       res.status(401).json({
         success: false,
-        msg: 'You are not authorized to visit this route' + err,
-      })
+        msg: "You are not authorized to visit this route" + err,
+      });
     }
   } else {
     res.status(401).json({
       success: false,
-      msg: 'You are not authorized to visit this route',
-    })
+      msg: "You are not authorized to visit this route",
+    });
   }
 }
 
-module.exports.genPassword = genPassword
-module.exports.verifyJwt = verifyJwt
+module.exports.genPassword = genPassword;
+module.exports.verifyJwt = verifyJwt;
